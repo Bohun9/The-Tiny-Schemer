158b47069a812e1a9fd66aeb9e26622ca457690f
Marcin Martowicz <marcinmartowicz9@gmail.com>
2023-01-28 18:50:00 +0100

    type inference test

--- test/test_typecheck.ml
+++ test/test_typecheck.ml
@@ -8,17 +8,66 @@ let read_whole_file filename =
   s
 ;;
 
+module M = Map.Make (String)
+
+let check_isomorphism t1 t2 =
+  let res = ref true in
+  let map0 = ref M.empty in
+  let map1 = ref M.empty in
+  let add m k v =
+    (match M.find_opt k !m with
+     | None -> ()
+     | Some w -> if v == w then () else res := false);
+    m := M.add k v !m
+  in
+  let rec run t1 t2 =
+    match t1, t2 with
+    | TInt, TInt -> ()
+    | TBool, TBool -> ()
+    | TUnit, TUnit -> ()
+    | TError, TError -> ()
+    | TList t1, TList t2 -> run t1 t2
+    | TCont t1, TCont t2 -> run t1 t2
+    | TRef t1, TRef t2 -> run t1 t2
+    | TPair (t11, t12), TPair (t21, t22) ->
+      run t11 t21;
+      run t12 t22
+    | TFun (t11, t12), TFun (t21, t22) ->
+      run t11 t21;
+      run t12 t22
+    | TVar x, TVar y ->
+      add map0 x y;
+      add map1 y x
+    | _ -> res := false
+  in
+  run t1 t2;
+  !res
+;;
+
 let parse_make n f e =
-  n >:: fun _ -> assert_equal (Schemer.Typecheck.typeof (Schemer.Interp.parse (read_whole_file f))) e
+  n
+  >:: fun _ ->
+  assert_bool
+    "what is this"
+    (check_isomorphism
+       (Schemer.Typecheck.typeof (Schemer.Interp.parse (read_whole_file f)))
+       e)
 ;;
 
 let fib_type = TInt
 let id_type = TInt
 
+let typeinfer_type =
+  TFun
+    ( TFun (TVar "x", TFun (TVar "y", TVar "z"))
+    , TFun (TFun (TVar "x", TVar "y"), TFun (TVar "x", TVar "z")) )
+;;
+
 let type_suite =
   "type inference"
   >::: [ parse_make "1" "code/fib" fib_type
        ; parse_make "2" "code/id" id_type
+       ; parse_make "3" "code/typeinfer" typeinfer_type
        ]
 ;;
 
